0cdab88abd2b73bb3e92426bcddbeb6c12d0d1c4
serbedzijajovan <jovajoksa@gmail.com>
2022-06-23 23:03:31 +0200

    Finished service implementation

---
+++ TollBoothManagementSystem/Core/Features/UserManagement/Service/IShiftService.cs
@@ -0,0 +1,9 @@
+﻿using TollBoothManagementSystem.Core.Features.UserManagement.Model;
+using TollBoothManagementSystem.Core.Utility;
+
+namespace TollBoothManagementSystem.Core.Features.UserManagement.Service
+{
+    public interface IShiftService : ICrudRepository<Shift>
+    {
+    }
+}
