e84ddb736ccaa796aaac8f72dffe078c9d6a2d53
ForLoop111 <vlada.devic.2001@gmail.com>
2022-06-23 22:02:25 +0200

    Added Shift Repository

---
+++ TollBoothManagementSystem/Core/Features/UserManagement/Repository/ShiftRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TollBoothManagementSystem.Core.Features.UserManagement.Model;
+using TollBoothManagementSystem.Core.Persistence;
+using TollBoothManagementSystem.Core.Utility;
+
+namespace TollBoothManagementSystem.Core.Features.UserManagement.Repository
+{
+    public class ShiftRepository : CrudRepository<Shift>, IShiftRepository
+    {
+        public ShiftRepository(DatabaseContext context) : base(context)
+        {
+        }
+    }
+}
