e6f8c73d554b3f5c153564165720d0260b28b247
jokicjovan <jokic.sv47.2020@uns.ac.rs>
2022-06-23 22:01:23 +0200

    updated repository

---
+++ TollBoothManagementSystem/Core/Features/UserManagement/Repository/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using TollBoothManagementSystem.Core.Features.UserManagement.Model;
+using TollBoothManagementSystem.Core.Persistence;
+using TollBoothManagementSystem.Core.Utility;
+
+namespace TollBoothManagementSystem.Core.Features.UserManagement.Repository
+{
+    public class UserRepository : CrudRepository<User>, IUserRepository
+    {
+        public UserRepository(DatabaseContext context) : base(context)
+        {
+
+        }
+    }
+}
